5406f7d1d686114ae87d42e2770779047e02b4f0
Debbl <me@aiwan.run>
2022-03-12 22:08:45 +0800

    Add AllInOne

---
+++ src/components/AllInOne.jsx
@@ -0,0 +1,141 @@
+import React from 'react';
+import { CheckIcon } from '@heroicons/react/outline';
+
+export default function AllInOne() {
+  return (
+    <div name="platforms" className="my-32 w-full">
+      <div className="px-2 mx-auto max-w-[1240px]">
+        <h2 className="text-5xl font-bold text-center">All-In-One Platform</h2>
+        <p className="py-8 text-2xl text-center text-gray-500">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque quos
+          nobis quam quae laudantium aspernatur minus ducimus quo explicabo.
+          Accusantium saepe doloremque culpa, atque consequatur deleniti
+          assumenda quasi possimus suscipit.
+        </p>
+
+        <div className="grid gap-4 pt-4 sm:grid-cols-2 lg:grid-cols-4">
+          {/* one */}
+          <div className="flex">
+            <div>
+              <CheckIcon className="mr-4 w-7 text-green-600" />
+            </div>
+            <div>
+              <h3 className="text-lg font-bold">Notifications</h3>
+              <p className="pt-2 pb-4 text-lg">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error
+                veniam eius ad, sint natus facilis nulla esse similique ullam
+                eveniet odit harum aut dolores, tenetur quibusdam, velit
+                officiis maxime debitis?
+              </p>
+            </div>
+          </div>
+          {/* two */}
+          <div className="flex">
+            <div>
+              <CheckIcon className="mr-4 w-7 text-green-600" />
+            </div>
+            <div>
+              <h3 className="text-lg font-bold">Notifications</h3>
+              <p className="pt-2 pb-4 text-lg">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error
+                veniam eius ad, sint natus facilis nulla esse similique ullam
+                eveniet odit harum aut dolores, tenetur quibusdam, velit
+                officiis maxime debitis?
+              </p>
+            </div>
+          </div>
+          {/* three */}
+          <div className="flex">
+            <div>
+              <CheckIcon className="mr-4 w-7 text-green-600" />
+            </div>
+            <div>
+              <h3 className="text-lg font-bold">Notifications</h3>
+              <p className="pt-2 pb-4 text-lg">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error
+                veniam eius ad, sint natus facilis nulla esse similique ullam
+                eveniet odit harum aut dolores, tenetur quibusdam, velit
+                officiis maxime debitis?
+              </p>
+            </div>
+          </div>
+          {/* four */}
+          <div className="flex">
+            <div>
+              <CheckIcon className="mr-4 w-4 text-green-600" />
+            </div>
+            <div>
+              <h3 className="text-lg font-bold">Notifications</h3>
+              <p className="pt-2 pb-4 text-lg">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error
+                veniam eius ad, sint natus facilis nulla esse similique ullam
+                eveniet odit harum aut dolores, tenetur quibusdam, velit
+                officiis maxime debitis?
+              </p>
+            </div>
+          </div>
+          {/* five */}
+          <div className="flex">
+            <div>
+              <CheckIcon className="mr-4 w-4 text-green-600" />
+            </div>
+            <div>
+              <h3 className="text-lg font-bold">Notifications</h3>
+              <p className="pt-2 pb-4 text-lg">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error
+                veniam eius ad, sint natus facilis nulla esse similique ullam
+                eveniet odit harum aut dolores, tenetur quibusdam, velit
+                officiis maxime debitis?
+              </p>
+            </div>
+          </div>
+          {/* six */}
+          <div className="flex">
+            <div>
+              <CheckIcon className="mr-4 w-4 text-green-600" />
+            </div>
+            <div>
+              <h3 className="text-lg font-bold">Notifications</h3>
+              <p className="pt-2 pb-4 text-lg">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error
+                veniam eius ad, sint natus facilis nulla esse similique ullam
+                eveniet odit harum aut dolores, tenetur quibusdam, velit
+                officiis maxime debitis?
+              </p>
+            </div>
+          </div>
+          {/* seven */}
+          <div className="flex">
+            <div>
+              <CheckIcon className="mr-4 w-4 text-green-600" />
+            </div>
+            <div>
+              <h3 className="text-lg font-bold">Notifications</h3>
+              <p className="pt-2 pb-4 text-lg">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error
+                veniam eius ad, sint natus facilis nulla esse similique ullam
+                eveniet odit harum aut dolores, tenetur quibusdam, velit
+                officiis maxime debitis?
+              </p>
+            </div>
+          </div>
+          {/* eight */}
+          <div className="flex">
+            <div>
+              <CheckIcon className="mr-4 w-4 text-green-600" />
+            </div>
+            <div>
+              <h3 className="text-lg font-bold">Notifications</h3>
+              <p className="pt-2 pb-4 text-lg">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error
+                veniam eius ad, sint natus facilis nulla esse similique ullam
+                eveniet odit harum aut dolores, tenetur quibusdam, velit
+                officiis maxime debitis?
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
